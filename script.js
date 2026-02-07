9b862a380d0ecf3d24eb4113b88f414ff97e6e32
tenali28 <tenalilikhitha28@gmail.com>
2026-02-07 20:47:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
- // NO button movement
-function moveNo() {
-  const btn = document.querySelector(".no");
-  btn.style.position = "absolute";
-  btn.style.left = Math.random() * 80 + "vw";
-  btn.style.top = Math.random() * 80 + "vh";
+ function goYes() {
+  window.location.href = "story.html";
 }
 
-// Firecrackers on proposal page
-if (window.location.pathname.includes("proposal.html")) {
-  for (let i = 0; i < 30; i++) {
-    setTimeout(() => {
-      const fire = document.createElement("div");
-      fire.className = "fire";
-      fire.innerHTML = "🎆";
-      fire.style.left = Math.random() * 100 + "vw";
-      fire.style.top = Math.random() * 100 + "vh";
-      document.body.appendChild(fire);
-
-      setTimeout(() => fire.remove(), 1800);
-    }, i * 120);
-  }
+function moveNo() {
+  const noBtn = document.querySelector(".no");
+  noBtn.style.position = "absolute";
+  noBtn.style.left = Math.random() * 80 + "%";
+  noBtn.style.top = Math.random() * 80 + "%";
 }
